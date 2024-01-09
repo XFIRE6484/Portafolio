49293ccaccb89cac288ff7d6696a807cd0b3d761
EO <edgard.orellana@cit.hn>
2024-01-08 23:31:58 -0600

    Carousel Dynamic Image Full screen Size

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,21 +7,70 @@ import { FirebaseService } from 'src/services/firebase.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit
+{
   proyects: Proyect[] = [];
 
-  constructor(private firebaseService: FirebaseService) { }
-  async ngOnInit(): Promise<void> {
-    (await this.firebaseService.getProyects()).forEach((doc) => {
+  constructor(private firebaseService: FirebaseService)
+  {
+    addEventListener("fullscreenchange", (event) =>
+    {
+      if (document.fullscreenElement)
+      {
+        console.log("Entered fullscreen mode!");
+      } else
+      {
+        console.log("Exited fullscreen mode.");
+        document.querySelectorAll('.sliderImage-fullscreen').forEach(img =>
+        {
+          img.classList.add('sliderImage');
+          img.classList.remove('sliderImage-fullscreen');
+        }
+        );
+      }
+    });
+  }
+  async ngOnInit(): Promise<void>
+  {
+    (await this.firebaseService.getProyects()).forEach((doc) =>
+    {
       const proyect = doc.data() as Proyect;
       this.proyects.push(proyect);
       console.table(this.proyects);
     });
   }
-  async openIframe(url: string) {
-    const iframe = `<iframe src="${url}" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>`;        
+  async openIframe(url: string)
+  {
+    const iframe = `<iframe src="${url}" width="100%" height="100%" frameborder="0" scrolling="no"></iframe>`;
     const newTab = window.open('', '_blank') as Window;
     newTab.document.body.innerHTML = iframe;
     //const client = await window.appetize.getClient("#appetize");
+  }
+
+  requestFullscreen(element: Element, id: number): void
+  {
+    const carousel = document.getElementById(`carousel${id}`);
+    if (carousel)
+    {
+      if (document.fullscreenElement)
+      {
+        document.exitFullscreen();
+        carousel.querySelectorAll('.sliderImage-fullscreen').forEach(img =>
+        {
+          img.classList.add('sliderImage');
+          img.classList.remove('sliderImage-fullscreen');
+        }
+        );
+      } else
+      {
+        carousel.requestFullscreen();
+        carousel.querySelectorAll('.sliderImage').forEach(img =>
+        {
+          img.classList.remove('sliderImage');
+          img.classList.add('sliderImage-fullscreen');
+        }
+        );
+      }
+    }
   }
 }
